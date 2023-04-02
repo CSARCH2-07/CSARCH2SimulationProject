439a930258213dbc8349f3d4f5a8491d3cd90d98
xam-ileto <xam.ileto@gmail.com>
2023-04-03 03:00:23 +0800

    setup sample home page html and web path

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Home page');
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port, () => {
