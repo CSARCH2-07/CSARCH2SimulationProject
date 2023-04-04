8ee67dc5b64054344bd580d6b73ee1a17b3f7db2
Sophia <sophia.oquias@gmail.com>
2023-04-04 20:09:07 +0800

    renamed vars

--- public/convert.js
+++ public/convert.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     function getPointIndex(num) {
-        for(i = 0; i < num.length; i++) {
-            if(num[i] === '.') {
+        for (i = 0; i < num.length; i++) {
+            if (num[i] === '.') {
                 return i
             }
         }
 
-        return -1; 
+        return -1;
     }
 
     function getLength(num) {
-        numLength = 0; 
+        numLength = 0;
         for (i = 0; i < num.length; i++) {
             if (num[i] >= '0' && num[i] <= '9') {
                 numLength++;
@@ -32,25 +32,25 @@ $(document).ready(function() {
     }
 
     function standardizePoint(num) {
-        num = num.slice(0, 1) + '.' + 
-            num.slice(1, getPointIndex(num)) + 
+        num = num.slice(0, 1) + '.' +
+            num.slice(1, getPointIndex(num)) +
             num.slice(getPointIndex(num) + 1, num.length);
 
-        return num; 
+        return num;
     }
 
     function getBinary(num) {
         var temp = parseInt(num);
         var result = ""
-        while(temp != 0) {
-            if(temp % 2 == 0) {
+        while (temp != 0) {
+            if (temp % 2 == 0) {
                 result = result.concat('0');
             }
             else {
                 result = result.concat('1');
             }
 
-            temp = Math.floor(temp / 2); 
+            temp = Math.floor(temp / 2);
         }
 
         result = result.split("");
@@ -70,77 +70,77 @@ $(document).ready(function() {
         var e = digit1[0];
         var i = digit2[0];
 
-        var aei = a + e + i; 
+        var aei = a + e + i;
 
         var bcd = 0000000000;
 
-        if(aei === '000') {
-            bcd = digit0.slice(1, digit0.length) + 
-                digit1.slice(1, digit1.length) + 
+        if (aei === '000') {
+            bcd = digit0.slice(1, digit0.length) +
+                digit1.slice(1, digit1.length) +
                 '0' +
                 digit2.slice(1, digit2.length);
         }
-        else if(aei === '001') {
+        else if (aei === '001') {
             bcd = digit0.slice(1, digit0.length) +
-                digit1.slice(1, digit1.length) + 
-                '100' + digit2[3]; 
+                digit1.slice(1, digit1.length) +
+                '100' + digit2[3];
         }
-        else if(aei === '010') {
+        else if (aei === '010') {
             bcd = digit0.slice(1, digit0.length) +
-                digit2.slice(1, 3) + digit1[3] + 
+                digit2.slice(1, 3) + digit1[3] +
                 '101' + digit2[3];
         }
-        else if(aei === '011') {
-            bcd = digit0.slice(1, digit0.length) + 
-                '10' + digit1[3] + 
+        else if (aei === '011') {
+            bcd = digit0.slice(1, digit0.length) +
+                '10' + digit1[3] +
                 '111' + digit2[3];
         }
-        else if(aei === '100') {
-            bcd = digit2.slice(1, 3) + digit0[3] + 
-                digit1.slice(1, digit1.length) + 
-                '110' + digit2[3]; 
+        else if (aei === '100') {
+            bcd = digit2.slice(1, 3) + digit0[3] +
+                digit1.slice(1, digit1.length) +
+                '110' + digit2[3];
 
         }
-        else if(aei === '101') {
-            bcd = digit1.slice(1, 3) + digit0[3] + 
-                '01' + digit1[3] + 
-                '111' + digit2[3]; 
+        else if (aei === '101') {
+            bcd = digit1.slice(1, 3) + digit0[3] +
+                '01' + digit1[3] +
+                '111' + digit2[3];
         }
-        else if(aei === '110') {
-            bcd = digit2.slice(1, 3) + digit0[3] + 
-                '00' + digit1[3] + 
-                '111' + digit2[3]; 
+        else if (aei === '110') {
+            bcd = digit2.slice(1, 3) + digit0[3] +
+                '00' + digit1[3] +
+                '111' + digit2[3];
         }
-        else if(aei === '111') {
-            bcd = '00' + digit0[3] + 
-                '11' + digit1[3] + 
-                '111' + digit2[3]; 
+        else if (aei === '111') {
+            bcd = '00' + digit0[3] +
+                '11' + digit1[3] +
+                '111' + digit2[3];
         }
-        
-        return bcd; 
+
+        return bcd;
     }
 
-    $("#submit").click(function() {
-        
+    $("#submit").click(function () {
+
         var num = parseFloat($("#num").val())
         var exp = parseInt($("#exponent").val())
 
-        if(!$("#num").val()) {
-            num = 0; 
+        if (!$("#num").val()) {
+            num = 0;
             $("#num").val('0');
         }
-        
+
         if (!$("#exponent").val()) {
             exp = 0;
             $("#exponent").val('0');
         }
 
-        var isPositive = true; 
+        var isPositive = true;
 
         // perform steps 
         // get sign 
-        if(num < 0) {
-            isPositive = false; 
+        if (num < 0) {
+            isPositive = false;
         }
 
         num = Math.abs(num);
@@ -150,8 +150,8 @@ $(document).ready(function() {
 
         // standardize 
         // extend 0's if needed 
-        if(numLength < 7) {
-            num = extend(num, 7); 
+        if (numLength < 7) {
+            num = extend(num, 7);
         }
 
         var pointIndex = getPointIndex(num);
@@ -159,79 +159,79 @@ $(document).ready(function() {
 
         // move decimal point if needed
         if (pointIndex != -1 && pointIndex >= 1) {
-            var numToMove = pointIndex - 1; 
-            exp = exp + numToMove; 
+            var numToMove = pointIndex - 1;
+            exp = exp + numToMove;
             // move point 
             num = standardizePoint(num); // TODO change to remove point 
-            digitAfterPoint = 2; 
+            digitAfterPoint = 2;
         }
 
         // get MSD
-        var MSD = extend(getBinary(num[0]), 4);
+        var msd = extend(getBinary(num[0]), 4);
 
         // get E 
-        var E = exp + 101; 
-        E = extend(getBinary(E.toString()), 8);
+        var ePrime = exp + 101;
+        ePrime = extend(getBinary(ePrime.toString()), 8);
 
         // get BCD 
 
         var bcd1 = toBCD(num.slice(digitAfterPoint, digitAfterPoint + 3));
         var bcd2 = toBCD(num.slice(digitAfterPoint + 3, num.length));
-        
-        if(isPositive) {
+
+        if (isPositive) {
             $("#sign").text("0");
         }
         else {
             $("#sign").text("1");
         }
 
-        if(MSD[0] === '0') {
-            var combinationField = E.slice(0, 3) + MSD.slice(1, MSD.length);
+        if (msd[0] === '0') {
+            var combinationField = ePrime.slice(0, 3) + msd.slice(1, msd.length);
         }
         else {
-            var combinationField = '11' + E.slice(0, 3) + MSD[3];
+            var combinationField = '11' + ePrime.slice(0, 3) + msd[3];
         }
 
-        var exponentContinuation = E.slice(2, E.length);
+        var exponentContinuation = ePrime.slice(2, ePrime.length);
 
         $("#combination-field").text(combinationField);
         $("#exponent-continuation").text(exponentContinuation);
         $("#bcd1").text(bcd1);
         $("#bcd2").text(bcd2);
     })
-	
-	$("#export").click(function() {
-		var inputNum = document.getElementById("num").value;
-		var inputExp = document.getElementById("exponent").value;
+
+    $("#export").click(function () {
+        var inputNum = document.getElementById("num").value;
+        var inputExp = document.getElementById("exponent").value;
         var outputSign = document.getElementById("sign").innerHTML;
         var outputComb = document.getElementById("combination-field").innerHTML;
         var outputExp = document.getElementById("exponent-continuation").innerHTML;
         var outputBCD1 = document.getElementById("bcd1").innerHTML;
         var outputBCD2 = document.getElementById("bcd2").innerHTML;
         var outputHex = document.getElementById("hex").innerHTML;
-		
-		var data = "";
-		data += "IEEE-754 Decimal-32 Floating-Point Converter" + "\n"
-		data += "---------------------------------------------------------------------" + "\n";
-		
-		data += "Input: " + inputNum + " x10^ " + inputExp + "\n";
-		data += "\n";	
-		
-		data += "Sign: " + outputSign + "\n";
-		data += "Combination Field: " + outputComb + "\n";
-		data += "Exponent Continuation: " + outputExp + "\n";
-		data += "First 10 BCD: " + outputBCD1 + "\n";
-		data += "Last 10 BCD: " + outputBCD2 + "\n";
-		data += "\n";	
-		
+
+        var data = "";
+        data += "IEEE-754 Decimal-32 Floating-Point Converter" + "\n"
+        data += "---------------------------------------------------------------------" + "\n";
+
+        data += "Input: " + inputNum + " x10^ " + inputExp + "\n";
+        data += "\n";
+
+        data += "Sign: " + outputSign + "\n";
+        data += "Combination Field: " + outputComb + "\n";
+        data += "Exponent Continuation: " + outputExp + "\n";
+        data += "First 10 BCD: " + outputBCD1 + "\n";
+        data += "Last 10 BCD: " + outputBCD2 + "\n";
+        data += "\n";
+
         data += "Hexadecimal Equivalent: " + outputHex;
 
         const link = document.createElement("a");
-        const file = new Blob([data],{type:"text/plain"});
+        const file = new Blob([data], { type: "text/plain" });
 
-		link.href = URL.createObjectURL(file);
-		link.download = 'DecimalToText.txt';
+        link.href = URL.createObjectURL(file);
+        link.download = 'DecimalToText.txt';
         link.click();
         URL.revokeObjectURL(link.href);
-	})		
+    })
 })
