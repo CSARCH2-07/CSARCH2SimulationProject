a4d27face0de9eb3a26d98acaa6808265011afeb
xam-ileto <xam.ileto@gmail.com>
2023-04-05 21:35:42 +0800

    added round to nearest ties to even

--- rounding-test.js
+++ rounding-test.js
@@ -1,20 +1,3 @@
-function getPointIndex(num) {
-  for (i = 0; i < num.length; i++) {
-    if (num[i] === '.') {
-      return i;
-    }
-  }
-
-  return -1;
-}
-
-function normalize(num) {
-  num =
-    num.slice(0, getPointIndex(num)) +
-    num.slice(getPointIndex(num) + 1, num.length);
-  return num;
-}
-
 function ApplyRounding(roundingMethod, num) {
   // num is a string
 
@@ -30,21 +13,31 @@ function ApplyRounding(roundingMethod, num) {
     if (roundingMethod === 'truncate') {
       return trimmedNumber;
     } else if (roundingMethod === 'ceiling') {
-      return Math.ceil(num);
+      return String(Math.ceil(num));
     } else if (roundingMethod === 'floor') {
-      return Math.floor(num);
+      return String(Math.floor(num));
     } else {
       // if round to nearest ties to even
-    }
+      var d = 0; // decimal places
+      var m = Math.pow(10, d);
+      var n = +(d ? num * m : num).toFixed(8); // Avoid rounding errors
+      var i = Math.floor(n),
+        f = n - i;
+      var e = 1e-8; // Allow for rounding errors in f
+      var r =
+        f > 0.5 - e && f < 0.5 + e ? (i % 2 == 0 ? i : i + 1) : Math.round(n);
 
-    // TODO should i return a string?
+      return String(d ? r / m : r);
+    }
   }
+
+  return num; // if < 7 length do not edit
 }
 
 // main
-num = '12345678';
+num = '12345665';
 console.log('RESULTS---------');
 console.log(ApplyRounding('truncate', num));
 console.log(ApplyRounding('ceiling', num));
 console.log(ApplyRounding('floor', num));
-// console.log(ApplyRounding('rtnte', num));
+console.log(ApplyRounding('rtnte', num));
